921ad98fbbec38ffa89292991f9608b4406ac0f4
AnatolSalau <salovanatoly5@mail.ru>
2022-11-10 16:18:35 +0300

    Integration Test

--- src/main/java/by/salov/tms/courseproject/repositories/UserJpaRepository.java
+++ src/main/java/by/salov/tms/courseproject/repositories/UserJpaRepository.java
@@ -4,10 +4,12 @@ import by.salov.tms.courseproject.entities.User;
 import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+@Transactional
 public interface UserJpaRepository extends JpaRepository<User,Long> {
     Optional<User> findUserByLogin (String login);
 
--- src/test/java/by/salov/tms/courseproject/dao/UserDBServiceTest.java
+++ src/test/java/by/salov/tms/courseproject/dao/UserDBServiceTest.java
@@ -2,13 +2,21 @@ package by.salov.tms.courseproject.dao;
 
 import by.salov.tms.courseproject.repositories.UserJpaRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+
+@SpringBootTest(webEnvironment =
+        SpringBootTest.WebEnvironment. RANDOM_PORT)
+@AutoConfigureMockMvc
+@Testcontainers
 class UserDBServiceTest {
 
+    @Autowired
     private UserJpaRepository userJpaRepository;
 
     @Test
